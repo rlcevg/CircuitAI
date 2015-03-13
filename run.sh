90bfe8076f938096721ebbff4e4a689182ccfc9f
rlcevg <rlcevg@gmail.com>
2015-03-13 13:33:19 +0200

    Update version {0.5}

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 make CircuitAI -j8
-cp AI/Skirmish/CircuitAI/libSkirmishAI.so ~/.spring/engine/98.0.1-test/AI/Skirmish/CircuitAI/0.4/
-echo "Done installation!"
+cp AI/Skirmish/CircuitAI/libSkirmishAI.so ~/.spring/engine/98.0.1-test/AI/Skirmish/CircuitAI/0.5/
+echo "Done!"
