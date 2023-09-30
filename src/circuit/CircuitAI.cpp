4189e91c1b3365753f9c1b15868c628b00509e86
rlcevg <514675+rlcevg@users.noreply.github.com>
2023-09-30 22:35:11 +0300

    Update version 1.6.8

--- src/circuit/CircuitAI.cpp
+++ src/circuit/CircuitAI.cpp
@@ -79,7 +79,7 @@ using namespace terrain;
  * Разрушать города,
  * Видеть в братьях мишени...
  */
-constexpr char version[]{"1.6.7"};
+constexpr char version[]{"1.6.8"};
 constexpr uint32_t VERSION_SAVE = 4;
 
 std::unique_ptr<CGameAttribute> CCircuitAI::gameAttribute(nullptr);
