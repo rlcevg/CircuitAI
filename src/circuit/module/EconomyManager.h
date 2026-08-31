f4a6ca3ae096631894e194cfa9b4e8e53f4fb78c
0x12A01DC <213792787+0x12A01DC@users.noreply.github.com>
2026-08-31 16:51:44 +0200

    Make `SPullMtoS` comparison operators const (#141)

--- src/circuit/module/EconomyManager.h
+++ src/circuit/module/EconomyManager.h
@@ -262,8 +262,8 @@ private:
 		float pull;
 		int mex;
 		float fraction;
-		inline bool operator< (const SPullMtoS& rhs) { return mex < rhs.mex; }
-		inline bool operator() (const SPullMtoS& lhs, const int rhs) { return lhs.mex < rhs; }
+		inline bool operator< (const SPullMtoS& rhs) const { return mex < rhs.mex; }
+		inline bool operator() (const SPullMtoS& lhs, const int rhs) const{ return lhs.mex < rhs; }
 	};
 	std::vector<SPullMtoS> mspInfos;
 	float pullMtoS = 0.f;  // mobile to static metal pull ratio
